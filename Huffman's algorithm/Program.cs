c546d672538282b27c2e3333d99f3a56b7b7fedf
kaneril <karmanova0ev@gmail.com>
2019-06-05 19:49:32 +0300

    сделан интерфейс взаимодействия с пользователем и функция сжатия данных алгоритмом хаффмана

---
+++ Huffman's algorithm/Program.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Huffman_s_algorithm
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:\n 1 Закодировать текст\n 2 Раскодировать текст\n 3 Завершить работу");
+                var str = Console.ReadLine();
+                switch (str)
+                {
+                    case "1":
+                        EncryptText();
+                        break;
+                    case "2":
+                        DecryptText();
+                        break;
+                    case "3":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Неверный код действия");
+                        break;
+                }
+            }
+        }
+
+        static void EncryptText()
+        {
+            var NormalText = ReadNormalText();
+            var symbols = ReadSymbols(NormalText);
+            Console.WriteLine($"{NormalText}");
+            symbols = MakeSymbolsTree(symbols);
+            Console.WriteLine("Введите путь к файлу с закодированным текстом");
+            var path = Console.ReadLine();
+            WriteInfo(symbols);
+            WriteSymbolsTree(path, symbols);
+            WriteEncryptText(path,  NormalText, symbols);
+        }
+        static void DecryptText()
+        {
+
+        }
+        static string ReadNormalText()
+        {
+            Console.WriteLine("Введите путь к файлу с кодируемым текстом");
+            var path = Console.ReadLine();
+            string NormalText = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    int s;
+                    while ((s = sr.Read()) != -1)
+                    {
+                        NormalText += (char)s;
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return NormalText;
+        }
+        static List<Symbol> ReadSymbols(string NormalText)
+        {
+            List<Symbol> symbols = new List<Symbol>();
+            foreach (var s in NormalText)
+            {
+                MakeSymbolsList(s, symbols);                
+            }                          
+            return symbols;
+        }
+        static void MakeSymbolsList(int s, List<Symbol> symbols)
+        {
+            foreach (var i in symbols)
+            {
+                if (i.name == s)
+                {
+                    i.frequency++;
+                    s = -1;
+                    break;
+                }
+            }
+            if (s != -1)
+            {
+                Symbol ss = new Symbol(s);
+                symbols.Add(ss);
+            }
+
+        }
+        static List<SymbolNode> MakeSymbolsNodeList(List<Symbol> symbols)
+        {
+            List<SymbolNode> SymbolsNodeList = new List<SymbolNode>();
+            foreach (var i in symbols)
+            {
+                SymbolsNodeList.Add(new SymbolNode(i));
+            }
+            symbols.Clear();
+            return SymbolsNodeList;
+        }
+        static void SymbolsNodeListInsert(List<SymbolNode> SymbolsNodeList, SymbolNode node)
+        {
+            int i ;
+            for (i = 0; i< SymbolsNodeList.Count;i++)
+            {
+                if (SymbolsNodeList[i].frequency>=node.frequency)
+                {
+                    break;                    
+                }                
+            }
+            SymbolsNodeList.Insert(i, node);
+        }
+        static List<Symbol> MakeSymbolsTree(List<Symbol> symbols)
+        {
+            symbols.Sort(new SymbolComparer());
+            var SymbolsNodeList = MakeSymbolsNodeList(symbols);
+            while((SymbolsNodeList.Count!=1))
+            {
+                var node = new SymbolNode(SymbolsNodeList[0], SymbolsNodeList[1]);
+                SymbolsNodeList.RemoveRange(0, 2);
+                SymbolsNodeListInsert(SymbolsNodeList, node);
+            }
+            foreach(var i in SymbolsNodeList[0].children)
+            {
+                Console.WriteLine($"{(char)i.name} {i.frequency} {i.code} {i.level}");
+            }
+            return SymbolsNodeList[0].children;
+        }
+        static void WriteSymbolsTree(string path, List<Symbol> symbols)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    foreach (var i in symbols)
+                    {
+                        var SymbolCode = Convert.ToString(i.name, 2);
+                        SymbolCode = new String ('0',8-SymbolCode.Length)+SymbolCode;
+                        sw.Write(i.level + i.code + SymbolCode);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static string FindSymbols(char s, List<Symbol> symbols)
+        {
+            foreach(var i in symbols)
+            {
+                if ((char)i.name==s)
+                {
+                    return i.code;
+                }
+            }
+            return "";
+        }
+        static void WriteEncryptText(string path, string NormalText, List<Symbol> symbols)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    for (var i = 0; i < NormalText.Length; i++)
+                    {
+                        var s =FindSymbols(NormalText[i],symbols);
+                        sw.Write(s);
+                        Console.WriteLine($"{s}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void WriteInfo(List<Symbol> symbols)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("info.txt", false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine($"Длина алфавита {symbols.Count}");
+                    foreach(var i in symbols)
+                    {
+                        sw.WriteLine($"Символ {(char)i.name} Частота {i.frequency} Код {i.code} Уровень {i.level}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+    class SymbolNode: IComparable<SymbolNode>
+    {
+        public int name { get; set; }
+        public int frequency { get; set; }
+        public List<Symbol> children { get; set; }
+        public SymbolNode(Symbol s)
+        {
+            name = -1;
+            frequency = s.frequency;
+            children = new List<Symbol>() { s };
+        }
+        public SymbolNode(SymbolNode s1, SymbolNode s2)
+        {
+            name = -1;
+            frequency = s1.frequency+s2.frequency;
+            foreach(var i in s1.children)
+            {
+                i.code = "0"+ i.code;
+                i.level = "0"+ i.level;
+            }
+            foreach (var i in s2.children)
+            {
+                i.code = "1" + i.code;
+                i.level = "0" + i.level;
+            }
+            children = new List<Symbol>() ;
+            children.AddRange(s1.children);
+            children.AddRange(s2.children);
+        }
+        public int CompareTo(SymbolNode s)
+        {
+            return this.name.CompareTo(s.name);
+        }
+        
+    }
+    class SymbolNodeComparer : IComparer<SymbolNode>
+    {
+        public int Compare(SymbolNode s1, SymbolNode s2)
+        {
+            if (s1.frequency > s2.frequency)
+                return 1;
+            else if (s1.frequency < s2.frequency)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    class Symbol : IComparable<Symbol>
+    {
+        public int name { get; set; }
+        public int frequency { get; set; }
+        public string code { get; set; }
+        public string level { get; set; }
+        public Symbol(int n) { name = n; frequency = 1; code = ""; level = "1"; }
+        public Symbol(int n, int f, string c) { name = n; frequency = f; code = c;}
+        public int CompareTo(Symbol s)
+        {
+            return this.name.CompareTo(s.name);
+        }
+        
+    }
+    class SymbolComparer : IComparer<Symbol>
+    {
+        public int Compare(Symbol s1, Symbol s2)
+        {
+            if (s1.frequency > s2.frequency)
+                return 1;
+            else if (s1.frequency < s2.frequency)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+   
+}
---
+++ Huffman's algorithm/Properties/tree.cs
@@ -0,0 +1,281 @@
+﻿/*using System;
+namespace BinTree
+{
+    class Symbol : IComparable<Symbol>
+    {
+        public int name { get; set; }
+        public int frequency { get; set; }
+        public string code { get; set; }
+        public Symbol(int n) { name = n; frequency = 1; code = ""; }
+        public Symbol(int n, int f, string c) { name = n; frequency = f; code = c; }
+        public int CompareTo(Symbol s)
+        {
+            return this.name.CompareTo(s.name);
+        }
+    }
+    class SymbolComparer : IComparer<Symbol>
+    {
+        public int Compare(Symbol s1, Symbol s2)
+        {
+            if (s1.frequency > s2.frequency)
+                return 1;
+            else if (s1.frequency < s2.frequency)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class Programm
+    {
+        /// <summary>
+        /// Расположения узла относительно родителя
+        /// </summary>
+        public enum Side
+        {
+            Left,
+            Right
+        }
+
+        /// <summary>
+        /// Узел бинарного дерева
+        /// </summary>
+        /// <typeparam Symbol="T"></typeparam>
+        public class BinaryTreeNode<T> where T : IComparable
+        {
+            /// <summary>
+            /// Конструктор класса
+            /// </summary>
+            /// <param name="data">Данные</param>
+            public BinaryTreeNode(T data)
+            {
+                Data = data;
+            }
+
+            /// <summary>
+            /// Данные которые хранятся в узле
+            /// </summary>
+            public T Data { get; set; }
+
+            /// <summary>
+            /// Левая ветка
+            /// </summary>
+            public BinaryTreeNode<T> LeftNode { get; set; }
+
+            /// <summary>
+            /// Правая ветка
+            /// </summary>
+            public BinaryTreeNode<T> RightNode { get; set; }
+
+            /// <summary>
+            /// Родитель
+            /// </summary>
+            public BinaryTreeNode<T> ParentNode { get; set; }
+
+            /// <summary>
+            /// Расположение узла относительно его родителя
+            /// </summary>
+            public Side? NodeSide =>
+                ParentNode == null
+                ? (Side?)null
+                : ParentNode.LeftNode == this
+                    ? Side.Left
+                    : Side.Right;
+
+            /// <summary>
+            /// Преобразование экземпляра класса в строку
+            /// </summary>
+            /// <returns>Данные узла дерева</returns>
+            public override string ToString() => Data.ToString();
+        }
+
+        /// <summary>
+        /// Бинарное дерево
+        /// </summary>
+        /// <typeparam name="T">Тип данных хранящихся в узлах</typeparam>
+        public class BinaryTree<T> where T : IComparable
+        {
+            /// <summary>
+            /// Корень бинарного дерева
+            /// </summary>
+            public BinaryTreeNode<T> RootNode { get; set; }
+        }
+
+        /// <summary>
+        /// Добавление нового узла в бинарное дерево
+        /// </summary>
+        /// <param name="node">Новый узел</param>
+        /// <param name="currentNode">Текущий узел</param>
+        /// <returns>Узел</returns>
+        public BinaryTreeNode<T> Add(BinaryTreeNode<T> node, BinaryTreeNode<T> currentNode = null)
+        {
+            if (RootNode == null)
+            {
+                node.ParentNode = null;
+                return RootNode = node;
+            }
+
+            currentNode = currentNode ?? RootNode;
+            node.ParentNode = currentNode;
+            int result;
+            return (result = node.Data.CompareTo(currentNode.Data)) == 0
+                ? currentNode
+                : result < 0
+                    ? currentNode.LeftNode == null
+                        ? (currentNode.LeftNode = node)
+                        : Add(node, currentNode.LeftNode)
+                    : currentNode.RightNode == null
+                        ? (currentNode.RightNode = node)
+                        : Add(node, currentNode.RightNode);
+        }
+
+        /// <summary>
+        /// Добавление данных в бинарное дерево
+        /// </summary>
+        /// <param name="data">Данные</param>
+        /// <returns>Узел</returns>
+        public BinaryTreeNode<T> Add(T data)
+        {
+            return Add(new BinaryTreeNode<T>(data));
+        }
+
+        /// <summary>
+        /// Поиск узла по значению
+        /// </summary>
+        /// <param name="data">Искомое значение</param>
+        /// <param name="startWithNode">Узел начала поиска</param>
+        /// <returns>Найденный узел</returns>
+        public BinaryTreeNode<T> FindNode(T data, BinaryTreeNode<T> startWithNode = null)
+        {
+            startWithNode = startWithNode ?? RootNode;
+            int result;
+            return (result = data.CompareTo(startWithNode.Data)) == 0
+                ? startWithNode
+                : result < 0
+                    ? startWithNode.LeftNode == null
+                        ? null
+                        : FindNode(data, startWithNode.LeftNode)
+                    : startWithNode.RightNode == null
+                        ? null
+                        : FindNode(data, startWithNode.RightNode);
+        }
+
+        /// <summary>
+        /// Удаление узла бинарного дерева
+        /// </summary>
+        /// <param name="node">Узел для удаления</param>
+        public void Remove(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            var currentNodeSide = node.NodeSide;
+            //если у узла нет подузлов, можно его удалить
+            if (node.LeftNode == null && node.RightNode == null)
+            {
+                if (currentNodeSide == Side.Left)
+                {
+                    node.ParentNode.LeftNode = null;
+                }
+                else
+                {
+                    node.ParentNode.RightNode = null;
+                }
+            }
+            //если нет левого, то правый ставим на место удаляемого 
+            else if (node.LeftNode == null)
+            {
+                if (currentNodeSide == Side.Left)
+                {
+                    node.ParentNode.LeftNode = node.RightNode;
+                }
+                else
+                {
+                    node.ParentNode.RightNode = node.RightNode;
+                }
+
+                node.RightNode.ParentNode = node.ParentNode;
+            }
+            //если нет правого, то левый ставим на место удаляемого 
+            else if (node.RightNode == null)
+            {
+                if (currentNodeSide == Side.Left)
+                {
+                    node.ParentNode.LeftNode = node.LeftNode;
+                }
+                else
+                {
+                    node.ParentNode.RightNode = node.LeftNode;
+                }
+
+                node.LeftNode.ParentNode = node.ParentNode;
+            }
+            //если оба дочерних присутствуют, 
+            //то правый становится на место удаляемого,
+            //а левый вставляется в правый
+            else
+            {
+                switch (currentNodeSide)
+                {
+                    case Side.Left:
+                        node.ParentNode.LeftNode = node.RightNode;
+                        node.RightNode.ParentNode = node.ParentNode;
+                        Add(node.LeftNode, node.RightNode);
+                        break;
+                    case Side.Right:
+                        node.ParentNode.RightNode = node.RightNode;
+                        node.RightNode.ParentNode = node.ParentNode;
+                        Add(node.LeftNode, node.RightNode);
+                        break;
+                    default:
+                        var bufLeft = node.LeftNode;
+                        var bufRightLeft = node.RightNode.LeftNode;
+                        var bufRightRight = node.RightNode.RightNode;
+                        node.Data = node.RightNode.Data;
+                        node.RightNode = bufRightRight;
+                        node.LeftNode = bufRightLeft;
+                        Add(bufLeft, node);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Удаление узла дерева
+        /// </summary>
+        /// <param name="data">Данные для удаления</param>
+        public void Remove(T data)
+        {
+            var foundNode = FindNode(data);
+            Remove(foundNode);
+        }
+
+        /// <summary>
+        /// Вывод бинарного дерева
+        /// </summary>
+        public void PrintTree()
+        {
+            PrintTree(RootNode);
+        }
+
+        /// <summary>
+        /// Вывод бинарного дерева начиная с указанного узла
+        /// </summary>
+        /// <param name="startNode">Узел с которого начинается печать</param>
+        /// <param name="indent">Отступ</param>
+        /// <param name="side">Сторона</param>
+        private void PrintTree(BinaryTreeNode<T> startNode, string indent = "", Side? side = null)
+        {
+            if (startNode != null)
+            {
+                var nodeSide = side == null ? "+" : side == Side.Left ? "L" : "R";
+                Console.WriteLine($"{indent} [{nodeSide}]- {startNode.Data}");
+                indent += new string(' ', 3);
+                //рекурсивный вызов для левой и правой веток
+                PrintTree(startNode.LeftNode, indent, Side.Left);
+                PrintTree(startNode.RightNode, indent, Side.Right);
+            }
+        }
+    }
+}*/
